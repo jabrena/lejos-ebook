1154ebafb4ccdaaa7fe50b79c18c1ff98d939528
archivojab@gmail.com <archivojab@gmail.com@11ee5571-0048-f3c9-5cf0-7f810f4828dc>
2011-11-13 16:54:52 +0000

    I have added this simple examples used to test your LeJOS installation

---
+++ trunk/sources/annexes/src/HelloWorld.java
@@ -0,0 +1,13 @@
+import lejos.nxt.*; 
+
+public class HelloWorld { 
+
+    /** 
+     * @param args 
+     */ 
+    public static void main(String[] args) { 
+        System.out.println("Hello World"); 
+        Button.waitForAnyPress(); 
+    } 
+} 
+
---
+++ trunk/sources/annexes/src/Test.java
@@ -0,0 +1,8 @@
+	public class Test {
+		
+		public static void main(String[] arg) { 
+			System.out.print("My Java installation runs nice!\n"); 
+		} 
+
+	} 
+
