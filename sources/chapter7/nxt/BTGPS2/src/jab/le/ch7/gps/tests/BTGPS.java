5d5720147ce0e56202d3d9d3ce59965b5ce5da34
archivojab@gmail.com <archivojab@gmail.com@11ee5571-0048-f3c9-5cf0-7f810f4828dc>
2011-11-20 14:34:00 +0000

    I have updated the example

--- sources/chapter7/nxt/BTGPS2/src/jab/le/ch7/gps/tests/BTGPS.java
+++ sources/chapter7/nxt/BTGPS2/src/jab/le/ch7/gps/tests/BTGPS.java
@@ -97,13 +97,13 @@ public class BTGPS{
 		LCD.drawString("Searching...", 0, 0);
 		LCD.refresh();
 		//Make an BT inquire to get all Devices with BT Services enable
-		Vector devList = Bluetooth.inquire(5, 10,cod);
+		ArrayList<RemoteDevice> devList = Bluetooth.inquire(5, 10,cod);
 
 		//If exist GPS Devices near
 		if (devList.size() > 0){
 			String[] names = new String[devList.size()];
 			for (int i = 0; i < devList.size(); i++) {
-				RemoteDevice btrd = ((RemoteDevice) devList.elementAt(i));
+				RemoteDevice btrd = ((RemoteDevice) devList.get(i));
 				names[i] = btrd.getFriendlyName(true);
 			}
 				
@@ -119,7 +119,7 @@ public class BTGPS{
 				//Menu 1: Show all BT Devices
 				selected = searchMenu.select();
 				if (selected >=0){
-					RemoteDevice btrd = ((RemoteDevice) devList.elementAt(selected));
+					RemoteDevice btrd = ((RemoteDevice) devList.get(selected));
 					LCD.clear();
 					LCD.drawString("Found",6,0);
 					LCD.drawString(names[selected],0,1);
