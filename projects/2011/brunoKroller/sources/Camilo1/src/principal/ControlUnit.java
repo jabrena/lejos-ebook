bfb757e58de8ed0fc409a385af2902af211cd588
gabun88 <gabun88@11ee5571-0048-f3c9-5cf0-7f810f4828dc>
2012-03-21 14:18:51 +0000

    Ajuste del diametro de ruedas para mejorar el giro

--- projects/2011/brunoKroller/sources/Camilo1/src/principal/ControlUnit.java
+++ projects/2011/brunoKroller/sources/Camilo1/src/principal/ControlUnit.java
@@ -1,8 +1,24 @@
+/*
+ * Name:ControlUnit
+ * Description: 
+ * Author: Bruno Kröller da Silva
+ * Date: 2011-2012
+*/
+
 package principal;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 
 import lejos.nxt.Button;
 import lejos.nxt.ButtonListener;
+import lejos.nxt.Motor;
 import lejos.nxt.SensorPort;
+import lejos.nxt.Sound;
 import lejos.nxt.UltrasonicSensor;
 import lejos.nxt.addon.CompassSensor;
 import modulos.GPSSensor;
@@ -30,47 +46,56 @@ public class ControlUnit implements ButtonListener{
 	public static void main(String args[]){
 	
 		//Variables
-		
 		ControlUnit listener = new ControlUnit();
 		UltrasonicSensor uSsensor;
 		CompassSensor cSensor;
-		boolean state = false;
+		Navigate nav = new Navigate(1, 4, Motor.A, Motor.C);
+				
+		boolean fstate = false;
 		int latitude = 0;
 		int longitude = 0;
 		int se = 0;
-		
+		double distance=0;
+		int i = 0 ;
 		//0-Initialize Buttons
-		
+		/*
 		Button.ENTER.addButtonListener(listener);
 		Button.RIGHT.addButtonListener(listener);
 		Button.LEFT.addButtonListener(listener);
 		System.out.println("ENTER = set");
 		System.out.println("RIGHT = begin");
 		System.out.println("ESCAPE = exit");
+		*/
+	
+		//16 caracteres por linea en pantalla
 		
+		Button.LEFT.addButtonListener(listener);
+		System.out.println("Hellow");
 		
 		/*
+		System.out.println("Linking GPS..   please wait");
 		GPSSensor gps= new GPSSensor(SensorPort.S3);
-		int i=0;
-		while(!state & i<100)
-		{
-			state=gps.linkStatus();
-			i++;
+		
+		while(!state) {
+			//Waiting for connection
+			if(gps.linkStatus()){
+				Sound.beep();
+				state=true;
+			}else // Connection success
+			{
+				System.out.println(gps.getUTC());
+				System.out.println("Waiting for GPS");
+				
+				
+			}
+			try {Thread.sleep(10000);} //Wait 10.000 miliseconds
+			catch (InterruptedException e) {	}			
 		}
-		if(!state)
-		{
-			listener.anyerror(1);
-			System.exit(0); //EXIT
-			
-		}
-		else{
-			System.out.println("GPS ON");
-			latitude=gps.getLatitude();
-			longitude=gps.getLongitude();
-			
-		}
+		
+		System.out.println("     GPS ON");
+		System.out.println(gps.getUTC());
+		System.out.println("----------------");
 		*/
-		System.out.println("-------");
 		
 		//Initialize Ultrasonic Sensor
 		uSsensor= new UltrasonicSensor(SensorPort.S1);
@@ -86,12 +111,49 @@ public class ControlUnit implements ButtonListener{
 		cSensor= new CompassSensor(SensorPort.S2);
 		System.out.println("Compass ON");
 		Button.waitForPress(); 
+		
+		/*LOG
+		File data = new File("log.dat");
+		int i2= 0;
+		try{
+			
+			OutputStream osw = new FileOutputStream (data);
+			DataOutputStream dos = new DataOutputStream(osw);
+			System.out.println("FILE OK");
+			
+		}
+		catch(IOException ioe)
+		{
+			System.err.println("FILE Error");
+		}
+		
+		latitude=gps.getLatitude();
+		longitude=gps.getLongitude();
+		
+		*/
+		
 		//---------------------------------------------ALGORITM---------------------------------------------
 		
 		
+				
+		while(!fstate)
+		{
+			nav.backward();
+			distance = uSsensor.getDistance();
+			if(distance<60)
+			{
+				nav.stop();
+				//state=true;
+				nav.Doodge(uSsensor);
+				i++;
+			}
+			if(i==4)
+				fstate=true;
+		}
 		
 		
-		
+		Button.waitForPress(); 
+		nav.stop();
 	
 	}//END MAIM
 	
